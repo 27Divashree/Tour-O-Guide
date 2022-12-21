634f1c9c5a2db791fd7089f68d12985189a3a157
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2022-12-21 16:28:07 +0530

    Sign in/up pop-up

--- js/main.js
+++ js/main.js
@@ -68,3 +68,11 @@
     
 })(jQuery);
 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
